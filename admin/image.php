1ae7c08aabe4587c0cccbd2258a7daa4fc3bcfbe
KhomenkoVlad <43212220+KhomenkoVlad@users.noreply.github.com>
2019-11-28 16:52:07 +0200

    add images

--- admin/image.php
+++ admin/image.php
@@ -6,10 +6,10 @@ if($_FILES["iimg"]["size"] > 1024*3*1024){
 }
 $new_name = $iname . '.png';
 // Проверяем загружен ли файл
-if(is_uploaded_file($_FILES["iimg"]["tmp_name"])){
-    // Если файл загружен успешно, перемещаем его
-    // из временной директории в конечную
+if(is_uploaded_file($_FILES["iimg"]["tmp_name"]) && isset($iname)){
+    // Если файл загружен успешно, перемещаем его из временной директории в конечную
     move_uploaded_file($_FILES["iimg"]["tmp_name"], "img/pizza/" . $new_name);
+    header("Location: admin.php");
 } else {
     echo("Ошибка загрузки файла");
 }
